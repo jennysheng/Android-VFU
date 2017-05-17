d0e6fe68d76ad08b191a6e5f4828ddab3846c527
jennysheng <qd_sheng2000@hotmail.com>
2017-05-17 11:56:03 +0200

    Pick a file from SD card using Intent

--- app/src/main/java/com/example/jenny_2/android_vfu/FileIO.java
+++ app/src/main/java/com/example/jenny_2/android_vfu/FileIO.java
@@ -1,5 +1,8 @@
 package com.example.jenny_2.android_vfu;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +13,8 @@ import android.widget.TextView;
 public class FileIO extends AppCompatActivity {
 
 
+    private static final int REQUESTCODE_PICK_TEXTFILE =1 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +22,25 @@ public class FileIO extends AppCompatActivity {
 
         final TextView dataText=(TextView)findViewById(R.id.dataText);
 
-
         final Button button2 = (Button) findViewById(R.id.Reade_externalButton);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                dataText.setText("Jenny");
+                //Pick a file from SD card using Intent
+                Intent textFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                textFileIntent.setType("text/*");
+                startActivityForResult(textFileIntent,REQUESTCODE_PICK_TEXTFILE);
             }
         });
+
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUESTCODE_PICK_TEXTFILE && resultCode == Activity.RESULT_OK) {
+            Uri textUri = data.getData();
+            Log.d("", "Text URI= " + textUri);
+        }
+    }
+
 }
