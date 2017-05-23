8aca4a37aa725704bb44a11e4a0949230a93ede0
jennysheng <qd_sheng2000@hotmail.com>
2017-05-23 10:28:17 +0200

    turn on and off graph ok in channel1

--- app/src/main/java/com/example/jenny_2/android_vfu/Scatterchart.java
+++ app/src/main/java/com/example/jenny_2/android_vfu/Scatterchart.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.github.mikephil.charting.charts.BarChart;
+
 import com.github.mikephil.charting.charts.ScatterChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
@@ -24,40 +25,58 @@ import java.util.ArrayList;
  * Created by Jenny_2 on 2017-05-21.
  */
 
-public class Scatterchart extends AppCompatActivity {
-    ArrayList<String> list1 = new ArrayList<>();
+public class Scatterchart extends AppCompatActivity implements View.OnClickListener {
+    ArrayList<String> list1;
+    Button btn1 = null;
+    ScatterChart scatterChart ;
+    ArrayList<Entry> entries ;
+    ArrayList<String> labels ;
+    ScatterDataSet dataset;
+    ScatterData data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scatterchart);
+        btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
 
-// get the value from the fileIO activity...........
+    }
 
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
+      @Override
+    public void onClick(View v) {
 
-        if (bundle != null) {
-            list1 = bundle.getStringArrayList("channel1data");
+        if (btn1.getText().equals("Channel1")) {
+
+            scatterChart = (ScatterChart) findViewById(R.id.chart);
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                list1 = bundle.getStringArrayList("channel1data");
+            }
+
+            entries = new ArrayList<>();
+            labels = new ArrayList<String>();
+            for (int i = 0; i < list1.size(); i++) {
+                entries.add(new Entry(Float.parseFloat(list1.get(i)), i));
+                labels.add("" + i);
+            }
+            dataset = new ScatterDataSet(entries, "channel1");
+            data = new ScatterData(labels, dataset);
+            dataset.setColors(new int[]{ColorTemplate.getHoloBlue()}); //
+            dataset.setScatterShapeSize(10);
+            dataset.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
+            scatterChart.setData(data);
+            scatterChart.animateX(30000);
+            btn1.setText("Off");
+
+        } else {
+            scatterChart.clearValues();
+            btn1.setText("Channel1");
+
         }
-        ScatterChart scatterChart = (ScatterChart) findViewById(R.id.chart);
-
-        ArrayList<Entry> entries = new ArrayList<>();
-        ArrayList<String> labels = new ArrayList<String>();
-        for(   int i=0; i<list1.size();i++) {
-            entries.add(new Entry(Float.parseFloat(list1.get(i)), i));
-            labels.add("Channel Values"+i);
-
-        }
-        ScatterDataSet dataset = new ScatterDataSet(entries, "# of Calls");
 
 
-
-        ScatterData data = new ScatterData(labels, dataset);
-        scatterChart.setData(data);
-        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
-        dataset.setScatterShapeSize(10);
-        dataset.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
-        scatterChart.animateY(5000);}
-
+    }
 }
+
